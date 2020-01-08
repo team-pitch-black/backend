6deaf88508684a1642264fc80f3f998b6e7914af
Bryan Szendel <bryan.szendel@gmail.com>
2020-01-07 17:11:18 -0700

    api-token-auth/ retrieves token

--- adv_project/urls.py
+++ adv_project/urls.py
@@ -1,9 +1,17 @@
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import path, include, re_path
 from django.conf.urls import include
+
+from rest_framework import routers
+from rest_framework.authtoken import views
+
+router = routers.DefaultRouter()
+# router.register('players')
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include('api.urls')),
     path('api/adv/', include('adventure.urls')),
+    path('api/users/', include('users.urls')),
+    re_path(r'^api-token-auth/', views.obtain_auth_token)
 ]
--- adventure/models.py
+++ adventure/models.py
@@ -1,6 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import User
-# from users.models import CustomUser
+from users.models import CustomUser
 from django.db.models.signals import post_save
 from django.dispatch import receiver
 from rest_framework.authtoken.models import Token
@@ -80,7 +80,7 @@ class Room(models.Model):
 
 
 class Player(models.Model):
-    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    user = models.OneToOneField(CustomUser, on_delete=models.CASCADE)
     uuid = models.UUIDField(default=uuid4, unique=True, editable=False)
     current_room = models.IntegerField(default=0)
     items = []
@@ -271,9 +271,9 @@ class World:
         # print(str)
 
 
-w = World()
-num_rooms = 44
-width = 8
-height = 7
-w.generate_rooms(width, height, num_rooms)
-w.print_rooms()
+# w = World()
+# num_rooms = 44
+# width = 8
+# height = 7
+# w.generate_rooms(width, height, num_rooms)
+# w.print_rooms()
