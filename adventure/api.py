f91160dfad067c8b7761b0c8be8cb1bb7c40d8fb
Bryan Szendel <bryan.szendel@gmail.com>
2020-01-08 13:28:34 -0700

    updated room_id response

--- adventure/api.py
+++ adventure/api.py
@@ -33,7 +33,7 @@ def initialize(request):
     uuid = player.uuid
     room = player.room()
     players = room.playerNames(player_id)
-    return JsonResponse({'uuid': uuid, 'name':player.user.username, 'room_type':room.room_type, 'description':room.description, 'players':players}, safe=True)
+    return JsonResponse({'uuid': uuid, 'name':player.user.username, 'room_id': room.id, 'room_type':room.room_type, 'description':room.description, 'players':players}, safe=True)
 
 
 """
@@ -82,10 +82,10 @@ def move(request):
         #     pusher.trigger(f'p-channel-{p_uuid}', u'broadcast', {'message':f'{player.user.username} has walked {dirs[direction]}.'})
         # for p_uuid in nextPlayerUUIDs:
         #     pusher.trigger(f'p-channel-{p_uuid}', u'broadcast', {'message':f'{player.user.username} has entered from the {reverse_dirs[direction]}.'})
-        return JsonResponse({'name':player.user.username, 'room_type':nextRoom.room_type, 'description':nextRoom.description, 'players':players, 'error_msg':""}, safe=True)
+        return JsonResponse({'name':player.user.username, 'room_id': nextRoom.id, 'room_type':nextRoom.room_type, 'description':nextRoom.description, 'players':players, 'error_msg':""}, safe=True)
     else:
         players = room.playerNames(player_id)
-        return JsonResponse({'name':player.user.username, 'room_type':room.room_type, 'description':room.description, 'players':players, 'error_msg':"You cannot move that way."}, safe=True)
+        return JsonResponse({'name':player.user.username, 'room_id': room.id, 'room_type':room.room_type, 'description':room.description, 'players':players, 'error_msg':"You cannot move that way."}, safe=True)
 
 
 @csrf_exempt
